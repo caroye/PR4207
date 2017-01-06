a1abcf4ec72dd23dab1b82de4ddc7a33c98bc4d7
caroye <elodie.caroy@edu.esiee.fr>
2017-01-06 11:34:50 +0100

    add change from 05/01/2017

--- main.cpp
+++ main.cpp
@@ -12,33 +12,31 @@
  */
 
 struct box2d_iterator;
+struct neighb2d_iterator;
 
 struct point2d{
-    //point2d() = default;
     int row;
     int col;
- //   string toString(){
-
- //   }
+    neighb2d_iterator createNIterator();
 };
 
 struct box2d{
-    box2d() = delete; //pas de box sans initialisation possible
-    bool has(point2d p){
-        return (p.row > pmin.row && p.row < pmax.row
-                && p.col >pmin.col && p.col < pmax.col);
+    box2d() = default;
+    bool has(point2d p) const{
+        return (p.row >= pmin.row && p.row < pmax.row+1
+                && p.col >=pmin.col && p.col < pmax.col+1);
     }
     point2d pmin, pmax;
-    box2d_iterator iterator(){
-        box2d_iterator i{*this};
-        return i;
-    }
+    box2d_iterator createIterator();
 };
 
 struct box2d_iterator{
-    box2d_iterator() = default;
     box2d d;
     point2d p;
+
+    void setBox(const box2d& b){
+        this->d = b;
+    }
 
     using value_type = point2d;
     value_type elt() const{
@@ -48,18 +46,16 @@ struct box2d_iterator{
         p = d.pmin;
     }
     bool isDone() const{
-        return (p.row == d.pmax.row && p.col == d.pmax.col);
+        return (p.row == d.pmax.row+1);// && p.col == d.pmax.col);
     }
+
     void next(){
-        if(this->isDone()!= true){
-            if(p.row != d.pmax.row)
-                p.row++;
-            else{
-                p.row = d.pmin.row;
+            if(p.col < d.pmax.col)
                 p.col++;
+            else{
+                p.col = d.pmin.col;
+                p.row++;
             }
-        }
-        //else
 
     }
     /*
@@ -68,23 +64,86 @@ struct box2d_iterator{
     }//conversion en point2d*/
 };
 
+
+box2d_iterator box2d::createIterator(){
+    box2d_iterator i;
+    i.setBox(*this);
+    return i;
+}
+
+
+
 /*template <typename T>
 struct image2d<T>{
     box2d d;
     std::vector<T> data;
 };*/
 
+
+struct neighb2d_iterator{
+    point2d p;
+    point2d ite;
+    void setPoint(const point2d& pt){
+        this->p = pt;
+    }
+
+    using value_type = point2d;
+    value_type elt() const{
+        return ite;
+    }
+    void start(){
+        ite.row = p.row-1;
+        ite.col = p.col;
+    }
+
+    bool isDone() const{
+        return (ite.row == p.row && ite.col == p.col+1);
+    }
+
+    void next(){
+        if(ite.row == p.row-1 && ite.col==p.col){
+            ite.row++;
+            ite.col--;
+        }
+        if(ite.row==p.row && ite.col==p.col-1){
+            ite.row++;
+            ite.col++;
+        }
+        if(ite.row==p.row+1 && ite.col==p.col){
+            ite.row--;
+            ite.col++;
+        }
+        if(ite.row==p.row && ite.col==p.col+1){
+            ite.row--;
+            ite.col--;
+        }
+    }
+};
+
+neighb2d_iterator point2d::createNIterator(){
+    neighb2d_iterator i;
+    i.setPoint(*this);
+    return i;
+}
+
 int main(int argc, char *argv[])
 {
-    point2d p{1,1}, pmin{0,0}, pmax{2,2}, p2{3,3};
+    point2d p{1,1}, pmin{0,0}, pmax{2,2}, p2{2,2};
     box2d D{pmin, pmax};
 
     std::cout << D.has(p) << std::endl;
     std::cout << D.has(p2) << std::endl;
 
-    auto i = D.iterator();
+    auto i = D.createIterator();
     for(i.start(); i.isDone() != true; i.next())
         std::cout << "(" << i.elt().row << "," << i.elt().col << ")" << " ";
+    std::cout << std::endl;
+
+    auto it = p.createNIterator();
+    for(it.start(); it.isDone(); it.next()){
+        std::cout << "plop" << std::endl;
+        std::cout << "(" << it.elt().row << "," << it.elt().col << ")" << " ";
+    }
     std::cout << std::endl;
 
     return 0;

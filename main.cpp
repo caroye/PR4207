a4083773e9a825b906de98ea965e7f4f668f3569
caroye <elodie.caroy@edu.esiee.fr>
2017-01-19 17:04:04 +0100

    dbt algo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+//#include <algorithm>
+#include <queue>
 
 /*
  * iterator{
@@ -29,6 +31,10 @@ struct box2d{
     }
     point2d pmin, pmax;
     box2d_iterator createIterator();
+
+    int nPoints(){
+        return ((pmax.col-pmin.col)*(pmax.row-pmax.row));
+    }
 };
 
 struct box2d_iterator{
@@ -59,9 +65,9 @@ struct box2d_iterator{
             }
 
     }
-    /*
-    void operator point2d() const{
 
+    operator point2d() const{
+        return p;
     }//conversion en point2d*/
 };
 
@@ -79,19 +85,22 @@ struct image2d{
     box2d d;
     std::vector<T> data;
 
-    image2d(box2d b/*, vector data*/)
-        : d(b)
+    image2d(box2d b/*, vector data*/): d(b)
     {
-        data.reserve(d.pmax.row*d.pmax.col);}
+        data.reserve(d.pmax.row*d.pmax.col);
+    }
 
     T operator()(const point2d& p) const{
         //return mat[j*this->i + i];
-        unsigned i= p.col*p.row+p.row;
+        unsigned i= (d.pmax.col+1)*p.row+p.row;
         return data[i];
     }
     T& operator()(point2d p){
-        unsigned i= p.col*p.row+p.row;
+        unsigned i= (d.pmax.col+1)*p.row+p.row;
         return data[i];
+    }
+    const box2d& domain() const{
+        return d;
     }
 };
 
@@ -142,8 +151,11 @@ neighb2d_iterator point2d::createIterator(){
     return i;
 }
 
+
+
 int main(int argc, char *argv[])
 {
+    /*
     point2d p{1,1}, pmin{0,0}, pmax{2,2}, p2{2,2};
     box2d D{pmin, pmax};
 
@@ -164,13 +176,13 @@ int main(int argc, char *argv[])
     std::cout << std::endl;
     std::cout << "plop2" << std::endl;
 
+    */
 
-    /*
-    ///////////////////////////////Algo//////////////////////////////////////
+    using bool_t = int ;
+    ///////////////////////////////Test image//////////////////////////////////////
 
     srand((unsigned int)time(NULL));
     int j;
-    using bool_t = int ;
     point2d pmin2{0,0}, pmax2{3,3};
     box2d dom{pmin2,pmax2};
     bool_t tab[16];
@@ -181,31 +193,85 @@ int main(int argc, char *argv[])
     }
 
     std::cout << "2" << std::endl;
-    //std::vector<bool_t> vec;
     image2d<bool_t> im{dom};
     std::cout << "3" << std::endl;
     auto ite = dom.createIterator();
     std::cout << "5" << std::endl;
     for(j=0; j<16; j++){
         for(ite.start(); ite.isDone() != true; ite.next()){
-            //std::cout << "6" << std::endl;
-            //std::cout << ite.elt().row << ", " << ite.elt().col << std::endl;
             im(ite.elt())=tab[j];
-            //std::cout << "7" << std::endl;
         }
     }
     std::cout << "4" << std::endl;
+
     for(ite.start(); ite.isDone() != true; ite.next())
         std::cout << "(" << ite.elt().row << "," << ite.elt().col << ") = " << im(ite.elt()) << " , ";
-    std::cout <<"plop"<< std::endl;
+    std::cout << std::endl;
 
     //*/
 
+    /////////////////////////////////////////////ALGO////////////////////////////////
+    bool_t val[16] = {1,1,1,0,0,0,1,1,0,1,0,0,1,0,1,1};
+    /*int taille = sizeof(val)/sizeof(int);
+    std::cout << "taille =" << taille <<std::endl;
+    int max = *std::max_element(val, val+taille);
+    std::cout << "max =" << max <<std::endl;*/
 
+/*
+    point2d pmin{0,0}, pmax{3,3};
+    box2d dom{pmin,pmax};
+    image2d<bool_t> input{dom};
+    auto i = dom.createIterator();
+    int j = 0;
+    for(j=0; j<16; j++){
+        for(i.start(); i.isDone() != true; i.next()){
+            std::cout << j ;
+            //input(i.elt())= val[j];
+        }
+    }
+/*
+    box2d D = input.domain();
+    std::cout << D.pmin.col << "," << D.pmax.col << std::endl;
+    const unsigned max = D.nPoints();
+    image2d<unsigned> dmap{D};
 
+/*    box2d_iterator p = D.createIterator();
+    for(p.start(); p.isDone() != true; p.next()){
+        dmap((point2d)p)= max;///////////////////PB!!!!!!!!!!
+    }
 
+    std::queue<point2d> q;
+    neighb2d_iterator n;
 
+    for(p.start(); p.isDone() != true; p.next()){
+        if(input(p.elt()) == 1){
+            dmap(p.elt()) = 0;
+            n.setPoint(p.elt());
+            for(n.start(); n.isDone() != true; n.next()){
+                if(D.has(n.elt()) && input(n.elt()) == false){
+                    q.push(p.elt());
+                    break;
+                }
+            }
+        }
+    }
 
+    while(!q.empty()){
+        point2d pnt = q.front();
+        q.pop();
+        n.setPoint(pnt);
+        for(n.start(); n.isDone() != true; n.next()){
+            if(D.has(n.elt()) && dmap(n.elt()) == max){
+                dmap(n.elt()) = dmap(pnt)+1;
+                q.push(n.elt());
+            }
+        }
+    }
+
+    for(p.start(); p.isDone() != true; p.next())
+        std::cout << "(" << p.elt().row << "," << p.elt().col << ") = " << dmap(p.elt()) << " , ";
+    std::cout << std::endl;
+    //*/
 
     return 0;
 }
